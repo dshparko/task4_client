85967e2b597027e0046ed6d0a0081d5513b7c1cc
Lenovo <d.shparko@mail.ru>
2022-11-25 20:29:02 +0300

    feat: add login page

---
+++ src/login/Login.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./login.css";
+import {Link} from "react-router-dom";
+
+const Login = () => {
+
+
+    return (
+        <div className="login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello World.</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+                        alias totam numquam ipsa exercitationem dignissimos, error nam,
+                        consequatur.
+                    </p>
+                    <span>Don't you have an account?</span>
+                    <Link to="/register">
+                        <button>Register</button>
+                    </Link>
+                </div>
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            name="username"
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                        />
+                        <button >Login</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
